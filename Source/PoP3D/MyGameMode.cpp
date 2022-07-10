ad7599fc957991f5d827de2556cab38c8953f6d2
DESKTOP-UOPN8LK\pinva <arclonerholdon@gmail.com>
2022-07-10 04:13:28 +0300

    Added ermac model and updated torches

---
+++ Source/PoP3D/MyGameMode.cpp
@@ -0,0 +1,11 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyGameMode.h"
+
+void AMyGameMode::InitGame(const FString& MapName, const FString& Options, FString& ErrorMessage)
+{
+	Super::InitGame(MapName, Options, ErrorMessage);
+	UE_LOG(LogTemp, Warning, TEXT("Hello world"));
+	UGameplayStatics::OpenLevel(this, "World'/Game/Library/Map/Prison.Prison'", false);
+}
